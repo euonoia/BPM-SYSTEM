780498c1940c1eedbdd78ede604627aa075968cc
Ken Española <revas4192@gmail.com>
2025-12-10 18:38:43 +0800

    initial

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('index');
 });
@@ -11,7 +10,3 @@ Route::get('/hr2', function () {
     return view('hr2.index');  
 })->name('hr2');
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/', [AuthController::class, 'dashboard'])->name('dashboard')->middleware('auth.employee');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
